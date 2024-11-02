29498e31637a3f09b44ee21f2edc78852c996e43
sallesyasmin <00001106694855sp@al.educacao.sp.gov.br>
2024-11-02 14:58:54 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,38 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+ 'Geografia' ,
+ 'Qual a capital do Iraque?',
+ 'A capital do Iraque é Bagdá'
+)
+criaCartao(
+    'História' ,
+    'Quando foi a revolução Pernanbucana?' ,
+    'A revolução Pernanbucana foi em 1817'
+)
+criaCartao(
+    'Matemática' ,
+    'Como é a formula de Bhaskhara?' ,
+    'A formula de Baskhara é x=-b+-√b.b-4a.c/2a'
+)
